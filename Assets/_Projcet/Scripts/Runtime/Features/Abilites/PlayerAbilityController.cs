821adac023a0efeef23dee7054c23d2032045ed2
PawsedDevelopment <pawseddevelopment@gmail.com>
2025-10-05 14:32:31 -0400

    Added ability swap for the UI elements

--- Assets/_Projcet/Scripts/Runtime/Features/Abilites/PlayerAbilityController.cs
+++ Assets/_Projcet/Scripts/Runtime/Features/Abilites/PlayerAbilityController.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerAbilityController : MonoBehaviour
 {
     [Header("Abilities")]
     [SerializeField] private AbilitySO _primaryAbility;
     [SerializeField] private AbilitySO _secondaryAbility;
+    [SerializeField] private Image primaryImage;
+    [SerializeField] private string primaryDesc;
+    [SerializeField] private string primaryName;
+    [SerializeField] private Image secondaryImage;
+    [SerializeField] private string secondaryDesc;
+    [SerializeField] private string secondaryName;
 
     private void Update()
     {
@@ -34,8 +41,24 @@ public class PlayerAbilityController : MonoBehaviour
     public void SwapAbilities(AbilitySO newPrimary, AbilitySO newSecondary)
     {
         _primaryAbility = newPrimary;
+        SetPrimaryInfo();
         _secondaryAbility = newSecondary;
+        SetSecondaryInfo();
 
         Debug.Log($"[AbilityController] Equipped: {newPrimary?.AbilityName} / {newSecondary?.AbilityName}");
+    }
+
+    public void SetPrimaryInfo()
+    {
+        primaryImage = _primaryAbility.Icon;
+        primaryDesc = _primaryAbility.Description;
+        primaryName = _primaryAbility.AbilityName;
+    }
+
+    public void SetSecondaryInfo()
+    {
+        secondaryImage = _secondaryAbility.Icon;
+        secondaryDesc = _secondaryAbility.Description;
+        secondaryName = _secondaryAbility.AbilityName;
     }
 }
---
+++ Assets/_Projcet/Scripts/Runtime/UI/AbilityUIUpdate.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class AbilityUIUpdate
+{
+    [SerializeField] private AbilitySO _primaryAbility;
+    [SerializeField] private AbilitySO _secondaryAbility;
+    [SerializeField] private Sprite primaryImage;
+    [SerializeField] private string primaryDesc;
+    [SerializeField] private string primaryName;
+    [SerializeField] private Sprite secondaryImage;
+    [SerializeField] private string secondaryDesc;
+    [SerializeField] private string secondaryName;
+
+    public void SetPrimary()
+    {
+
+    }
+
+    public void SetSecondary()
+    {
+
+    }
+
+
+
+
+}
