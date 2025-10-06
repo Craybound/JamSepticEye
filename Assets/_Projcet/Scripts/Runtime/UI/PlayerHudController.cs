94ae302433a38a63aecb8697f88c851982e8f0bb
Stas <stas.schnitzer@gmail.com>
2025-10-05 20:03:14 -0400

    Squashed commit of the following:
    commit 6e695c12ddddc7b920d45f01459b611b64d6478a
    Merge: 0ab7e05 f9b8311
    Author: Kaushik1128 <kaushikvp1128@gmail.com>
    Date:   Mon Oct 6 00:59:49 2025 +0100
    
        Merge branch 'Feature/MainMenu-Setup' of https://github.com/Craybound/JamSepticEye into Feature/MainMenu-Setup
    
    commit 0ab7e05271f4e17c320dded786aba869ef340f25
    Author: Kaushik1128 <kaushikvp1128@gmail.com>
    Date:   Mon Oct 6 00:57:49 2025 +0100
    
        UI connected with AbilitySO
    
    commit f9b831173fa7bb170d4a1877587e6d789f7879cb
    Author: Stas <stas.schnitzer@gmail.com>
    Date:   Sun Oct 5 19:44:07 2025 -0400
    
        Update Enemy_Ranged_Elite_PF.prefab
    
    commit 4cfef4bf7191ad65dc1d48a8423b673bb0bd4b1a
    Author: Stas <stas.schnitzer@gmail.com>
    Date:   Sun Oct 5 19:34:51 2025 -0400
    
        Fixed wave counter ui
    
    commit c743a9c22c2053fb9573174553b96827d5026c71
    Author: Stas <stas.schnitzer@gmail.com>
    Date:   Sun Oct 5 19:19:24 2025 -0400
    
        Player Hp working
    
    commit 950a12e020c8b376a510ebeaf31d8f239d57359e
    Author: Stas <stas.schnitzer@gmail.com>
    Date:   Sun Oct 5 18:51:15 2025 -0400
    
        fiex on hit with interactable
    
    commit 0ad729a7ac9b1dcec8e88e159a2eb6d9b782e443
    Author: Stas <stas.schnitzer@gmail.com>
    Date:   Sun Oct 5 17:38:15 2025 -0400
    
        Updated trigger colider
    
    commit 3a1dfd02a3e6bd4fa07eb145a7832b9e078e12ce
    Author: Stas <stas.schnitzer@gmail.com>
    Date:   Sun Oct 5 17:36:52 2025 -0400
    
        Dasher Kit working
    
    commit 579e476fbc3bfa6d17918b3f749ef3e331a70a63
    Author: Stas <stas.schnitzer@gmail.com>
    Date:   Sun Oct 5 17:27:18 2025 -0400
    
        projectile works
    
    commit 0fde1f712e475621526095e4815c1b84f8f247be
    Author: Stas <stas.schnitzer@gmail.com>
    Date:   Sun Oct 5 17:19:05 2025 -0400
    
        aoe ground zone dealing damage
    
    commit 7f5cfa18d772f8d625c65f436e90cb8674ea6a82
    Author: Stas <stas.schnitzer@gmail.com>
    Date:   Sun Oct 5 16:19:07 2025 -0400
    
        Squashed commit of the following:
    
        commit 3f0d0d5ca5cd550bd7c289268eb5d66e88d1b36c
        Author: Kaushik1128 <kaushikvp1128@gmail.com>
        Date:   Sun Oct 5 20:31:23 2025 +0100
    
            Complete Elite dasher ability, boss dasher ability, elite brute ability and boss brute ability.
    
        commit 0ff5776e47e03bb6cd004ac4bb9ca1bc9f0a703f
        Author: Kaushik1128 <kaushikvp1128@gmail.com>
        Date:   Sun Oct 5 16:03:46 2025 +0100
    
            Complete all the abilities, visuals are working but the damage is not registering
    
        commit 2c423087538416e4a3e015bf75b760b8ea2a1b99
        Author: Kaushik1128 <kaushikvp1128@gmail.com>
        Date:   Sun Oct 5 14:08:02 2025 +0100
    
            The elite dasher ability bug is fixed
    
    commit 117fcd02c7d66b132783207021afc881aea96150
    Author: Stas <stas.schnitzer@gmail.com>
    Date:   Sun Oct 5 16:08:49 2025 -0400
    
        Squashed commit of the following:
    
        commit 1f4a44a1486b323e24f0c111e1743d1fc6e167b0
        Author: Kaushik1128 <kaushikvp1128@gmail.com>
        Date:   Sat Oct 4 22:21:34 2025 +0100
    
            Fixed the Elite Dasher bug
    
        commit 6cbd768e8a07f9144c7970d316b2da5108111de6
        Author: Kaushik1128 <kaushikvp1128@gmail.com>
        Date:   Sat Oct 4 21:49:11 2025 +0100
    
            Got the dash bug fixed

--- Assets/_Projcet/Scripts/Runtime/UI/PlayerHudController.cs
+++ Assets/_Projcet/Scripts/Runtime/UI/PlayerHudController.cs
@@ -1,71 +1,96 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro; // use TMP if you prefer
 
 public class PlayerHudController : MonoBehaviour
 {
+    [Header("UI Root & Buttons")]
     [SerializeField] private GameObject _interactionUI;
     [SerializeField] private Button _killButton;
     [SerializeField] private Button _consumeButton;
+    [SerializeField] private PlayerAbilityController controller;
 
-    [SerializeField]private PlayerAbilityController controller;
+    [Header("Ability Display Texts")]
+    [SerializeField] private TMP_Text _primaryNameText;
+    [SerializeField] private TMP_Text _secondaryNameText;
+    [SerializeField] private TMP_Text _primaryDescText;
+    [SerializeField] private TMP_Text _secondaryDescText;
 
+    [Header("Ability Icons (Optional)")]
+    [SerializeField] private Image _primaryIcon;
+    [SerializeField] private Image _secondaryIcon;
 
-    private void OnEnable()
-    {
-        AbilityInteractable.OnInteracted += ShowInteractionUI;
-    }
-
-    private void OnDisable()
-    {
-        AbilityInteractable.OnInteracted -= ShowInteractionUI;
-    }
-
+    private void OnEnable()  => AbilityInteractable.OnInteracted += ShowInteractionUI;
+    private void OnDisable() => AbilityInteractable.OnInteracted -= ShowInteractionUI;
 
     private void Awake()
     {
-        if( _interactionUI != null )
-        _interactionUI.SetActive(false);
-        if (controller == null)
-        {
-            controller = GameObject.FindWithTag("Player").GetComponent<PlayerAbilityController>();
-        }
+        if (_interactionUI != null)
+            _interactionUI.SetActive(false);
 
+        if (controller == null)
+            controller = GameObject.FindWithTag("Player")?.GetComponent<PlayerAbilityController>();
     }
 
-    public void ShowInteractionUI(EnemyController enemy,AbilitySO primary, AbilitySO secondary)
+    public void ShowInteractionUI(EnemyController enemy, AbilitySO primary, AbilitySO secondary)
     {
         _interactionUI.SetActive(true);
 
-        if (_consumeButton != null)
+        // ===== TOP BOXES: Ability Names =====
+        if (_primaryNameText)
+            _primaryNameText.text = primary ? primary.AbilityName : "—";
+
+        if (_secondaryNameText)
+            _secondaryNameText.text = secondary ? secondary.AbilityName : "—";
+
+        // ===== BOTTOM BOXES: Descriptions =====
+        if (_primaryDescText)
+            _primaryDescText.text = primary ? primary.Description : "";
+
+        if (_secondaryDescText)
+            _secondaryDescText.text = secondary ? secondary.Description : "";
+
+        // ===== ICONS (optional) =====
+        if (_primaryIcon)
+        {
+            _primaryIcon.sprite = primary && primary.Icon != null ? primary.Icon.sprite : null;
+            _primaryIcon.enabled = _primaryIcon.sprite != null;
+        }
+
+        if (_secondaryIcon)
+        {
+            _secondaryIcon.sprite = secondary && secondary.Icon != null ? secondary.Icon.sprite : null;
+            _secondaryIcon.enabled = _secondaryIcon.sprite != null;
+        }
+
+        // ===== BUTTONS =====
+        if (_consumeButton)
         {
             _consumeButton.onClick.RemoveAllListeners();
             _consumeButton.onClick.AddListener(() =>
             {
                 controller.SwapAbilities(primary, secondary);
                 enemy.Die();
-                _interactionUI.SetActive(false);
-                Time.timeScale = 1f; // Resume time after choosing
+                CloseUI();
             });
         }
 
-        _killButton.onClick.RemoveAllListeners();
-        _killButton.onClick.AddListener(() =>
+        if (_killButton)
         {
-            _interactionUI.SetActive(false);
-            enemy.Die();
-            Time.timeScale = 1f;
-        });
+            _killButton.onClick.RemoveAllListeners();
+            _killButton.onClick.AddListener(() =>
+            {
+                enemy.Die();
+                CloseUI();
+            });
+        }
 
-
-
-
-        Time.timeScale = 0f; // Pause game while interaction UI is active
+        Time.timeScale = 0f; // pause game while choosing
     }
 
-
-
-
-
-
-
+    private void CloseUI()
+    {
+        if (_interactionUI) _interactionUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
 }
